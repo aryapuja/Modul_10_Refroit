c3cdb590c9f363289cfb88c8c117669c5a5e84e1
aryapuja <aryapuja2103@gmail.com>
2018-11-16 19:25:39 +0700

    Codingan Tugas

---
+++ Praktek/app/src/main/java/com/rzn/praktekmodul10/ClickListener.java
@@ -0,0 +1,8 @@
+package com.rzn.praktekmodul10;
+
+import android.view.View;
+
+public interface ClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
